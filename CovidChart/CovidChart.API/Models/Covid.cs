d91a0ca7d7ef35f12cdbd377800b0ca9a73b9325
emir balcı <emir.balci@demirorenteknoloji.com>
2021-01-16 18:10:19 +0300

    commit

---
+++ CovidChart/CovidChart.API/Models/Covid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidChart.API.Models
+{
+    public enum ECity
+    {
+        Istanbul = 1,
+        Yalova = 2,
+        Ankara = 3,
+        Kocaeli = 4,
+        Izmir = 5
+    }
+    public class Covid
+    {
+        public int Id { get; set; }
+        public ECity City { get; set; }
+        public int Count { get; set; }
+        public DateTime CovidDate { get; set; }
+    }
+}
